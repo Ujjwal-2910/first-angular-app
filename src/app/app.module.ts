3c2c0a31a1038f1c139df36530ba6885ccff6e68
Ujjwal-2910 <ujjwalagrawal29102001@gmail.com>
2024-04-19 09:21:25 +0530

    Created First Angular App and added page-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './page-header.component';
 
+// Decorator - NGModule
 @NgModule({
+  /* delaration - this section lists all the components and directives used/defined in the
+   * application. It is a comma separated list of values*/
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent
   ],
+  /* imports - lists all the external modules that are required in this module */
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  /* exports - list al the app module that will be available to
+   * other external applications - optional */
+  /* provider - lists all the services and pipes defined withing the application
+   * and these services listedhere will be injected by Angular where ever required. */
   providers: [],
+  /* bootstarp - only the top-most Module in Angular can define this section.
+  -> No other Module within the application can define it.
+  - */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
